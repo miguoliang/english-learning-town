160646956864131a4eebc7bb9f79402ebea3d017
Mi Guoliang <boymgl@qq.com>
2025-11-08 23:46:20 +0800

    Initial Commit

---
+++ EnglishLearningTown/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  EnglishLearningTown
+//
+//  Created by Frank Mi on 2025/11/8.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
